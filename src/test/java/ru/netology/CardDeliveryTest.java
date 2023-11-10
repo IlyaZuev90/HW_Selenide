d58bc8ad3e04b504b642f89c0ca4567faf93c994
Ilya_Zuev_90 <god_of_iwowi@mail.ru>
2023-11-11 02:17:18 +0400

    Added solution to the first task

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,3 +1,62 @@
 package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
 public class CardDeliveryTest {
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldBePositiveTestCard() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Са");
+        try {
+            Thread.sleep(5000);
+        } catch(InterruptedException ex) {}
+        $$(".menu-item__control").findBy(text("Самара")).click();
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException ex) {}
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException ex) {}
+        String currentDate = generateDate(3, "dd.MM.yyyy");
+        try {
+            Thread.sleep(5000);
+        } catch(InterruptedException ex) {}
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException ex) {}
+        $("[data-test-id='name'] input").setValue("Пушкин Александр");
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException ex) {}
+        $("[data-test-id='phone'] input").setValue("+79270000000");
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException ex) {}
+        $("[data-test-id='agreement']").click();
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException ex) {}
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + currentDate));
+        try {
+            Thread.sleep(5000);
+        } catch(InterruptedException ex) {}
+    }
 }
+
