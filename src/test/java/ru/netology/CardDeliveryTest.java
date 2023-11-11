73d95d7bb6985836944c53313202174fcaea5a23
Ilya_Zuev_90 <god_of_iwowi@mail.ru>
2023-11-11 14:56:42 +0400

    Added solution to the second task. Corrected solution to the first task

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,6 +1,7 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -10,6 +11,7 @@ import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
+
 public class CardDeliveryTest {
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
@@ -18,45 +20,53 @@ public class CardDeliveryTest {
     @Test
     public void shouldBePositiveTestCard() {
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Са");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        $("[data-test-id='city'] input").setValue("Самара");
         try {
             Thread.sleep(5000);
-        } catch(InterruptedException ex) {}
-        $$(".menu-item__control").findBy(text("Самара")).click();
+        } catch (InterruptedException ex) {
+        }
+        $("[data-test-id='date'] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
         try {
             Thread.sleep(2000);
-        } catch(InterruptedException ex) {}
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        try {
-            Thread.sleep(2000);
-        } catch(InterruptedException ex) {}
+        } catch (InterruptedException ex) {
+        }
         String currentDate = generateDate(3, "dd.MM.yyyy");
         try {
             Thread.sleep(5000);
-        } catch(InterruptedException ex) {}
+        } catch (InterruptedException ex) {
+        }
         $("[data-test-id='date'] input").sendKeys(currentDate);
         try {
             Thread.sleep(2000);
-        } catch(InterruptedException ex) {}
+        } catch (InterruptedException ex) {
+        }
         $("[data-test-id='name'] input").setValue("Пушкин Александр");
         try {
             Thread.sleep(2000);
-        } catch(InterruptedException ex) {}
+        } catch (InterruptedException ex) {
+        }
         $("[data-test-id='phone'] input").setValue("+79270000000");
         try {
             Thread.sleep(2000);
-        } catch(InterruptedException ex) {}
+        } catch (InterruptedException ex) {
+        }
         $("[data-test-id='agreement']").click();
         try {
             Thread.sleep(2000);
-        } catch(InterruptedException ex) {}
+        } catch (InterruptedException ex) {
+        }
         $("button.button").click();
         $(".notification__content")
                 .shouldBe(visible, Duration.ofSeconds(15))
                 .shouldHave(exactText("Встреча успешно забронирована на " + currentDate));
         try {
             Thread.sleep(5000);
-        } catch(InterruptedException ex) {}
+        } catch (InterruptedException ex) {
+        }
     }
 }
 
---
+++ src/test/java/ru/netology/CardDeliveryTestSecondTask.java
@@ -0,0 +1,251 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTestSecondTask {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    private String generateDateSecondType(int targetDay, int targetMonth, int targetYear, String pattern) {
+        return LocalDate.now().withDayOfMonth(targetDay).withMonth(targetMonth).withYear(targetYear).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+
+    @Test
+    public void shouldTestSelectingFromCitiesBySymbols() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("ка");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+        }
+        $$(".menu-item__control").findBy(text("Йошкар-Ола")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        $("[data-test-id='date'] input").sendKeys(Keys.SHIFT, Keys.HOME, Keys.DELETE);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        String currentDate = generateDate(3, "dd.MM.yyyy");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+        }
+        $("[data-test-id='date'] input").sendKeys(currentDate);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        $("[data-test-id='name'] input").setValue("Пушкин Александр");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        $("[data-test-id='phone'] input").setValue("+79270000000");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        $("[data-test-id='agreement']").click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на " + currentDate));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+        }
+    }
+
+    @Test
+    public void shouldTestSelectingFromCalendarOneWeek() {
+        open("http://localhost:9999");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+        }
+        $("[data-test-id='city'] input").sendKeys("ка");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+        }
+        $$(".menu-item__control").findBy(text("Йошкар-Ола")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        $(".icon-button__text").click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        int addDays = 7;
+        int currentDate = LocalDate.now().getDayOfMonth();
+        int borderDate = LocalDate.now().lengthOfMonth();
+        int targetDay = currentDate + addDays;
+        int targetMonth = LocalDate.now().getMonthValue();
+        int targetYear = LocalDate.now().getYear();
+        for (int i = 0; i < 12; i++) {
+
+            if (targetDay > borderDate) {
+
+                targetDay = targetDay - borderDate;
+                $("[data-step = '1']").click();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                }
+                LocalDate dateMonth = LocalDate.now();
+                LocalDate newMonth = dateMonth.plusMonths(1);
+                borderDate = newMonth.lengthOfMonth();
+                targetMonth = targetMonth + 1;
+                if (targetMonth > 12) {
+                    targetMonth = 1;
+                    targetYear = targetYear + 1;
+                }
+                if (targetDay <= borderDate) {
+                    $$("[data-day]").findBy(text(String.valueOf(targetDay))).click();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                    $("[data-test-id='name'] input").setValue("Пушкин Александр");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                    $("[data-test-id='phone'] input").setValue("+79270000000");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                    $("[data-test-id='agreement']").click();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                    $("button.button").click();
+                    $(".notification__title")
+                            .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                            .shouldHave(Condition.exactText("Успешно!"));
+                    String actualDate = generateDateSecondType(targetDay, targetMonth, targetYear, "dd.MM.yyyy");
+                    $(".notification__content")
+                            .shouldHave(Condition.text("Встреча успешно забронирована на " + actualDate));
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                    return;
+
+                }
+            }
+
+        }
+
+    }
+
+    @Test
+    public void shouldTestSelectingFromCalendarAnyDateWithinPeriodOfHundredDays() {
+        open("http://localhost:9999");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+        }
+        $("[data-test-id='city'] input").sendKeys("ка");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException ex) {
+        }
+        $$(".menu-item__control").findBy(text("Йошкар-Ола")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        $(".icon-button__text").click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+        int addDays = 90;
+        int currentDate = LocalDate.now().getDayOfMonth();
+        int borderDate = LocalDate.now().lengthOfMonth();
+        int targetDay = currentDate + addDays;
+        int targetMonth = LocalDate.now().getMonthValue();
+        int targetYear = LocalDate.now().getYear();
+        for (int i = 0; i < 12; i++) {
+
+            if (targetDay > borderDate) {
+
+                targetDay = targetDay - borderDate;
+                $("[data-step = '1']").click();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                }
+                LocalDate dateMonth = LocalDate.now();
+                LocalDate newMonth = dateMonth.plusMonths(1);
+                borderDate = newMonth.lengthOfMonth();
+                targetMonth = targetMonth + 1;
+                if (targetMonth > 12) {
+                    targetMonth = 1;
+                    targetYear = targetYear + 1;
+                }
+                if (targetDay <= borderDate) {
+                    $$("[data-day]").findBy(text(String.valueOf(targetDay))).click();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                    $("[data-test-id='name'] input").setValue("Пушкин Александр");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                    $("[data-test-id='phone'] input").setValue("+79270000000");
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                    $("[data-test-id='agreement']").click();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                    $("button.button").click();
+                    $(".notification__title")
+                            .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                            .shouldHave(Condition.exactText("Успешно!"));
+                    String actualDate = generateDateSecondType(targetDay, targetMonth, targetYear, "dd.MM.yyyy");
+                    $(".notification__content")
+                            .shouldHave(Condition.text("Встреча успешно забронирована на " + actualDate));
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                    return;
+
+                }
+            }
+
+        }
+    }
+}
+
